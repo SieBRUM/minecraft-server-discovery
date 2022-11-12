d13becbd65d7a2d00b982e7d0d5f63aa3bf61c1d
SieBrum <siebrenkraak@hotmail.com>
2022-11-12 20:56:11 +0100

    Containerize everything except scanner (:

--- backend/MinecraftServerDiscoveryApi/Program.cs
+++ backend/MinecraftServerDiscoveryApi/Program.cs
@@ -21,6 +21,7 @@ namespace MinecraftServerDiscoveryApi
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:5000/");
                 });
     }
 }
